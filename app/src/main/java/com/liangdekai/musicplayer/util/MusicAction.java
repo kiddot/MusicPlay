4b11e3a41317c488c64b4f43d1a008b38b003e1f
kiddot <kiddo1024@163.com>
2016-10-23 02:35:26 +0800

    polish

--- app/src/main/java/com/liangdekai/musicplayer/util/MusicAction.java
+++
@@ -1,8 +0,0 @@
-package com.liangdekai.musicplayer.util;
-
-/**
- * Created by asus on 2016/9/27.
- */
-public class MusicAction {
-    public static final String MUSIC_PLAY = "com.liangdekai.musicplayer.play";
-}
